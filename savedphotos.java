ffc677151fec08e2e89df1b082793b277d45b7cf
ashit23 <74149479+ashit23@users.noreply.github.com>
2021-05-05 08:33:26 -0700

    Add files via upload

---
+++ savedphotos.java
@@ -0,0 +1,15 @@
+package com.scanner.demo;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class savedphotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savedphotos);
+    }
+}
